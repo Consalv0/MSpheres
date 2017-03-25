53f512d566fc9a4f17e17bf0a267c151a266749e
Consalvo <gonzaloceciliano@gmail.com>
2017-03-24 18:54:39 -0600

    First push

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+$('html, body').css({
+    overflow: 'hidden',
+    height: '100%'
+});
+$(document).on('click', 'a.smooth', function (e) {
+  e.preventDefault()
+  var $link = $(this)
+  var anchor = $link.attr('href')
+  if ($(anchor).outerHeight(true) <= $(window).height() - navbarHeight/2) {
+    $('html, body').stop().animate({
+      scrollTop: (($(window).height() + $(anchor).outerHeight(true))/2) - $(window).height() + $(anchor).position().top - navbarHeight/2
+    }, 1000)
+  } else {
+      $('html, body').stop().animate({
+        scrollTop: $(anchor).position().top - navbarHeight
+      }, 1000)
+  }
+})
