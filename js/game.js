1ec88cae701d6e9b93d46fb511e961dd931c0753
Consalvo <gonzaloceciliano@gmail.com>
2017-03-29 00:11:47 -0600

    Fixed Collision, New Webs Display

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
-/* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0 */
-/* global width, height, RGB, HSB, createVector, createCanvas, ellipseMode, ellipse,
-noStroke, line, BLEND, MULTIPLY, rectMode, CENTER, loadSound, keyCode ellipseMode,
-rect, RADIUS, random, DIFFERENCE, fill, keyIsDown, colorMode, frameRate, blendMode,
-textFont, textSize, text, fill, abs, stroke */
+/* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0, no-trailing-spaces: 0 */
+/* global width, height, RGB, HSB, createVector, createCanvas, ellipseMode, ellipse, noStroke,
+ line, BLEND, MULTIPLY, rectMode, CENTER, loadSound, keyCode ellipseMode, rect, RADIUS, random,
+ DIFFERENCE, fill, keyIsDown, colorMode, frameRate, blendMode, textFont, textSize, text, fill,
+ abs, stroke, strokeWeight, print */
 
 var file = []
 var spheres = []
@@ -12,43 +12,47 @@ var TRAIL = 50   // Tail Duration in ms
 var MAXSPH = 500  // Max number of spheres
 var MAXRAD = 18   // Max Sphere radius
 var MINRAD = 3    // Min Sphere radius
-var radSize
 var LAYER = [1, 2, 3, 4, 5]   // Max layers
-var sphLayer = 1  // Num layers
+var FRICTION = 10000 // More is less, <1 is none
 var numsounds = LAYER.length  // Define the number of samples
+var pointR = 3 // Radius of the preview sphere
 
 var HALFW
 var HALFH
+var sphLayer
+var radSize
 
-var FRICTION = 10000 // More is less, <1 is none
 var fCount = 0
 var keyTimer = 0
 
-var point; var pointR
+var point
 var offset
 var shot
 
 // eslint-disable-next-line
 function preload() {
-  for (var i = 0; i < numsounds; i++) {
+  for (let i = 0; i < numsounds; i++) {
     file.push(loadSound('assets/' + (i+1) + '.ogg'))
   }
 }
 
 // eslint-disable-next-line
 function setup() {
+  // createCanvas($(window).width(), $(window).height()).parent('game')
   createCanvas(960, 144).parent('game')
   rectMode(CENTER)
   ellipseMode(RADIUS)
+  strokeWeight(2)
 
-  HALFW = width*0.5
-  HALFH = height*0.5
+  sphLayer = random(LAYER)  // Starting layer
+  HALFW = width *0.5
+  HALFH = height *0.5
   offset = createVector(0, 0)
   point = createVector(HALFW, HALFH)
-  pointR = 3
 
-  for (var i = 5; i > 0; i--) {
-    spheres.push(new Sphere(createVector(random(width), random(height)), random(MINRAD, MAXRAD), createVector(random(10), random(10)), spheres.length, random(LAYER)))
+  for (let i = 5; i > 0; i--) {
+    spheres.push(new Sphere(createVector(random(width), random(height)), random(MINRAD, MAXRAD),
+     createVector(random(10), random(10)), spheres.length, random(LAYER)))
   }
   setInterval(soundTimer, 10)
   setInterval(radTimer, 10)
@@ -58,13 +62,13 @@ function setup() {
 function draw() {
   frameRate(60)
 
-  for (let i = preview.length - 1; i >= 0; i--) {
-    var prv = preview[i]
-    prv.display()
-    prv.placement()
+  for (let i = 0; i < preview.length; i++) {
+     var prv = preview[i]
+     prv.display()
+     prv.placement()
   }
 
-  for (let i = spheres.length - 1; i >= 0; i--) {
+  for (let i = 0; i < spheres.length; i++) {
     var sph = spheres[i]
     sph.movement()
     sph.display()
@@ -86,7 +90,7 @@ function draw() {
     if (keyIsDown(38)) {
       keyTimer++
       if (keyTimer > 100) {
-        for (var i = spheres.length; i >= 0; i--) {
+        for (let i = spheres.length; i >= 0; i--) {
           spheres.pop(i)
         }
       }
@@ -94,7 +98,8 @@ function draw() {
   }
   if (keyIsDown(32)) {
     if (preview.length <= 0) {
-      preview.push(new Sphere(createVector(point.x, point.y), radSize, createVector(0, 0), preview.length, sphLayer))
+      preview.push(new Sphere(createVector(point.x, point.y), radSize,
+       createVector(0, 0), preview.length, sphLayer))
     }
     shot = createVector(offset.x - point.x, offset.y - point.y)
   }
@@ -125,8 +130,8 @@ function keyPressed() {
     offset = createVector(point.x, point.y)
   }
   if (keyIsDown(40) && keyIsDown(38)) {
-    if (spheres.length - 1 > -1) {
-      spheres.pop(preview.length - 1)
+    if (spheres.length -1 > -1) {
+      spheres.pop(preview.length -1)
     }
     blendMode(BLEND)
     colorMode(HSB, 360, 100, 100)
@@ -134,7 +139,7 @@ function keyPressed() {
     rect(HALFW, HALFH, width, height)
   }
   if (keyIsDown(39) && keyIsDown(37)) {
-    if (sphLayer <= LAYER.length+1) {
+    if (sphLayer <= LAYER.length +1) {
       sphLayer++
       if (sphLayer > LAYER.length) {
         sphLayer = 1
@@ -142,7 +147,7 @@ function keyPressed() {
     }
     blendMode(BLEND)
       colorMode(HSB, 360, 100, 100)
-        fill(abs(sphLayer*360/LAYER.length), 100, 100)
+        fill(abs(sphLayer *360 /LAYER.length), 100, 100)
           rect(HALFW, HALFH, width, height)
   }
 }
@@ -156,7 +161,8 @@ function keyReleased() {
       preview.pop(preview.length)
     }
     if (spheres.length-1 <= MAXSPH) {
-      spheres.push(new Sphere(createVector(point.x, point.y), radSize, createVector(shot.x, shot.y), spheres.length, sphLayer))
+      spheres.push(new Sphere(createVector(point.x, point.y), radSize,
+       createVector(shot.x, shot.y), spheres.length, sphLayer))
     }
   }
 }
@@ -179,18 +185,10 @@ function Sphere(position, radius, velocity, id, layer) {
   var p = position; var r = radius; var playSound
   var v = velocity; var sphCollision
 
-  this.layer = function() {
-    return layer
-  }
-  this.p = function() {
-    return p
-  }
-  this.r = function() {
-    return r
-  }
-  this.v = function() {
-    return v
-  }
+  this.layer = function() { return layer }
+  this.p = function() { return p }
+  this.r = function() { return r }
+  this.v = function() { return v }
 
   this.placement = function() {
     // Preview of shotting sphere
@@ -206,6 +204,7 @@ function Sphere(position, radius, velocity, id, layer) {
 
   this.movement = function() {
     // Movement of Sphere
+    // Limit the speed of the spheres by it's size
     if (v.x > r) {
       v.normalize(); v.mult(r)
     } else if (v.x < -r) { v.normalize(); v.mult(r) }
@@ -226,8 +225,8 @@ function Sphere(position, radius, velocity, id, layer) {
     // Display of Sphere
     noStroke()
     blendMode(BLEND)
-      colorMode(HSB, 360, 100, 100)
-        fill(abs(layer *360 / LAYER.length), (abs(v.y) + abs(v.x)) *100 / (r *2) +25, 100)
+      colorMode(HSB, 360, 1, 1, 1)
+        fill(layer *72, (abs(v.y) + abs(v.x)) /(r +r) +0.25, 1, 1)
           ellipse(p.x, p.y, r, r)
   }
 
@@ -236,22 +235,22 @@ function Sphere(position, radius, velocity, id, layer) {
     if (p.x + r > width) {
       p.x += -abs(-width +p.x +r)
       v.x *= -1
-      playSound(layer, 0.5, r /MAXRAD)
+      playSound(layer, 1, r /MAXRAD)
     }
     if (p.x - r < 0) {
       p.x += abs(p.x -r)
       v.x *= -1
-      playSound(layer, 1, r /MAXRAD)
+      playSound(layer, 2, r /MAXRAD)
     }
     if (p.y + r > height) {
       p.y += -abs(-height +p.y +r)
       v.y *= -1
-      playSound(layer, 1.5, r /MAXRAD)
+      playSound(layer, 3, r /MAXRAD)
     }
     if (p.y - r < 0) {
       p.y += abs(p.y -r)
       v.y *= -1
-      playSound(layer, 2, r /MAXRAD)
+      playSound(layer, 4, r /MAXRAD)
     }
     /* Draw vector axis
     stroke(0, 100, 100)
@@ -260,13 +259,16 @@ function Sphere(position, radius, velocity, id, layer) {
       line(p.x, p.y, p.x, v.y * 10 + p.y)
     noStroke()
     */
+    // print('Esfera ' + id + ':')
     for (let j = id + 1; j < spheres.length; j++) { // Select all spheres pairs without repeating
       let sphOther = spheres[j]
-      // print(id + " y " + j);
+
+      // print(id + ' y ' + j)
       sphCollision(sphOther)
     }
   }
   sphCollision = function (sphOther) {
+    // print(id + ', ' + sphOther.id)
     if (layer !== sphOther.layer()) return
 
     let pi = p; let pj = sphOther.p()
@@ -276,12 +278,11 @@ function Sphere(position, radius, velocity, id, layer) {
     let vctBtw = createVector(pi.x - pj.x, pi.y - pj.y) // Vector created from the distance between them
     let disBtw = vctBtw.mag() // Magnitude of vctBtw
 
-    if (disBtw > sumRad * ri * 0.6) return // Minim distance to display web
+    let webDist = sumRad *8
+    if (disBtw > webDist) return // Minim distance to display web
 
-    colorMode(HSB, 360, 100, 100)
-      stroke(layer *72, (abs(v.y) + abs(v.x)) *100 /sumRad +10, 100) // ((x1)/x2*2 + (x3 + x4)/x1-0.5)/1.5
-        blendMode(BLEND)
-          line(pi.x, pi.y, pj.x, pj.y)
+    stroke(layer *72, (abs(v.y) +abs(v.x)) /sumRad +0.1, 1, (webDist -disBtw) /webDist)
+      line(pi.x, pi.y, pj.x, pj.y)
     noStroke()
 
     if (disBtw > sumRad-0.01) return // Too to far to collide
@@ -296,26 +297,25 @@ function Sphere(position, radius, velocity, id, layer) {
     }
 
     pi.add(vctBtw) // pi.add( vctBtw.mult( ri / sumRad ) );
-    pj.sub(vctBtw) // pj.add( vctBtw.mult( rj / sumRad ) );
+    pj.sub(vctBtw) // pj.sub( vctBtw.mult( rj / sumRad ) );
 
     let vi = v
     let vj = sphOther.v()
 
     let dirBtw = vctBtw.normalize() // Direction of the collision vector
 
-    let viM = vi.mag() // Magnitude of velocities
-    let vjM = vi.mag()
-
-    vi.set(dirBtw.mult(-viM)) // Change direction of the Spheres
-    vj.set(dirBtw.mult(-vjM))
-
-    // TODO Fix Collion when velocities are in the same direction
-    // let vk = createVector(vi.x+vj.x, vi.y+vj.y); // Save sum velocities before collision
+    // TODO Fix Collision Speed
     let vik = createVector(vi.x, vi.y) // Save velocities before collision
     let vjk = createVector(vj.x, vj.y)
 
-    vi.set(vjk.sub(vik)) // Invert directions
-    vj.set(vik.sub(vjk))
+    vi.set(vik.add(vjk)) // Compare velocities
+    vj.set(vjk.add(vik))
+
+    let viM = vi.mag() // Save new magnitudes
+    let vjM = vj.mag()
+
+    vi.set(dirBtw.mult(viM)) // Invert directions
+    vj.set(dirBtw.mult(-vjM))
 
     // TODO Make velocity proportional to the spheres rad
     // vi.mult(rj*2 / sumRad); // Make velocity proportional to the spheres rad
@@ -340,7 +340,7 @@ function Sphere(position, radius, velocity, id, layer) {
 
     ellipse(p.x, p.y, sumRad *0.7, sumRad *0.7)
 
-    playSound(layer, ri / sumRad *2, (ri + rj) /MAXRAD *2)
+    // playSound(layer, ri / sumRad *2, (ri + rj) /MAXRAD *2)
   }
 
   playSound = function (layer, rate, vol) {
--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('nav').outerHeight();
+/* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0 */
+var navbarHeight = $('nav').outerHeight()
 $('html, body').css({
-    overflow: 'hidden',
-    height: '100%'
-});
+  overflow: 'hidden',
+  height: '100%'
+})
 $(document).on('click', 'a.smooth', function (e) {
   e.preventDefault()
   var $link = $(this)
   var anchor = $link.attr('href')
-  if ($(anchor).outerHeight(true) <= $(window).height() - navbarHeight/2) {
+  if ($(anchor).outerHeight(true) <= $(window).height() - navbarHeight / 2) {
     $('html, body').stop().animate({
-      scrollTop: (($(window).height() + $(anchor).outerHeight(true))/2) - $(window).height() + $(anchor).position().top - navbarHeight/2
+      scrollTop: (($(window).height() + $(anchor).outerHeight(true)) / 2) - $(window).height() + $(anchor).position().top - navbarHeight/2
     }, 1000)
   } else {
       $('html, body').stop().animate({
